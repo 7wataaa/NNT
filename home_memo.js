96ab56648f85a40f12718a7367e39109e4acf762
444aaa <nn.contact.n@gmail.com>
2020-05-22 12:20:51 +0900

    コメントを変更

--- home_memo.js
+++ home_memo.js
@@ -1,15 +1,16 @@
 /**最初に読み込みたいものを放り込んでおく
  * 
- * localStrage.memo0 に保存されている文字列を memoExport と textarea#memo0 に出力する 
+ * 1.localStrage.memo0 に保存されている文字列を memoExport と textarea#memo0 に出力する 
+ * 
  */
 const startScript = () => {
-    if(localStorage.memo0 === ''){//ここの条件分岐はlocalstorageの内容を判定､memoExportかlocalstorageの書き換えを行う
+    if(localStorage.memo0 === ''){//ここの条件分岐はlocalstorageの内容を判定､memoExportもしくはlocalstorageの書き換えを行う
         document.getElementById('memoExport').innerText = ''
     
     }else if(localStorage.memo0.match(/(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/)){
 
         const Exportreplace = localStorage.memo0.replace(/(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g, '<a href="$&">$&</a>').replace(/\r?\n/g, '<br>')
-        document.getElementById('memoExport').innerHTML = Exportreplace//memoExportのリンクに<a>を追加する
+        document.getElementById('memoExport').innerHTML = Exportreplace//memoExportのリンクにアンカー追加､改行を<br>に変更
     }else{
         document.getElementById('memoExport').innerText = localStorage.memo0
     };
@@ -43,10 +44,6 @@ const copy0 = () => {
     document.getElementById('memo0').select();
     document.execCommand('copy');
 }
-//const paste0 = () => {
-//    
-//    };
 document.getElementById('save').addEventListener('click', save0)
 document.getElementById('reset').addEventListener('click', reset0)
 document.getElementById('copy').addEventListener('click', copy0)
-//document.getElementById('paste').addEventListener('click', paste0)
