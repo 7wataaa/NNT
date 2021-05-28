9cbc135fe8ec7abc4a7ea4cdc77544926a6929ee
nashi <nn.contact.n@gmail.com>
2021-05-28 13:55:35 +0900

    fix: 文字列内にリンクが入力されていたときの動作を修正

--- home_memo.js
+++ home_memo.js
@@ -2,83 +2,113 @@
  * ・undefined対策 ・localStorageからmemoExportへ(必要があれば変換してから)出力する
  */
 const startScript = () => {
-    if (localStorage.memo0 == undefined) {
-        localStorage.memo0 = ''
-        startScript();
-    } else if (/(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g.test(localStorage.memo0)) {
+  if (localStorage.memo0 == undefined) {
+    localStorage.memo0 = "";
+    startScript();
+  } else if (
+    /https?:\/\/[\w!\?/\+\-_~=;\.,\*&@#\$%\(\)'\[\]]+/g.test(localStorage.memo0)
+  ) {
+    document.getElementById("memoExport").textContent = "";
 
-        const Exportreplace = localStorage.memo0.replace(/(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g, '<a href="$&">$&</a>').replace(/\r?\n/g, '<br>')
+    const memoStr = localStorage.memo0;
 
-        document.getElementById('memoExport').innerHTML = Exportreplace
+    const splits = memoStr.split(
+      /(https?:\/\/[\w!\?/\+\-_~=;\.,\*&@#\$%\(\)'\[\]]+)/g
+    );
 
-    } else {
-        document.getElementById('memoExport').innerText = localStorage.memo0
+    for (const str of splits) {
+      if (/https?:\/\/[\w!\?/\+\-_~=;\.,\*&@#\$%\(\)'\[\]]+/g.test(str)) {
+        const anchor = document.createElement("a");
+        anchor.href = str;
+        anchor.innerText = str;
+
+        document
+          .getElementById("memoExport")
+          .insertAdjacentElement("beforeend", anchor);
+      } else {
+        document
+          .getElementById("memoExport")
+          .insertAdjacentText("beforeend", str);
+      }
     }
+  } else {
+    document.getElementById("memoExport").innerText = localStorage.memo0;
+  }
 
-    document.getElementById('memo0').value = localStorage.memo0;
-}
+  document.getElementById("memo0").value = localStorage.memo0;
+};
 
 /**
  * memo0(テキストエリア) の内容を localStorage.memo0 に保存して再読み込みする
  */
 const save0 = () => {
-    localStorage.memo0 = document.getElementById('memo0').value;
-    startScript()
-}
+  localStorage.memo0 = document.getElementById("memo0").value;
+  startScript();
+};
 
 /**
  * ・テキストエリアに文字がある時→同期する内容を上書きする・テキストエリアになにもない時→登録されている文字を読み込む、なければはじめの文を登録して再帰
  */
 const sync0 = () => {
-    if (document.getElementById('memo0').value === '') {
+  if (document.getElementById("memo0").value === "") {
+    chrome.storage.sync.get("syncedNote", (result) => {
+      if (!result.syncedNote) {
+        chrome.storage.sync.set(
+          {
+            syncedNote:
+              "syncは約10KB, saveは約5MBまで保存できます\n10KBは約5000文字です",
+          },
+          () => {
+            sync0();
+          }
+        );
+      } else {
+        localStorage.memo0 = result.syncedNote;
+        startScript();
+        console.log("syncedNoteを読み込みました");
+      }
+    });
+  } else {
+    save0();
 
-        chrome.storage.sync.get("syncedNote", result => {
-            if (!result.syncedNote) {
-                chrome.storage.sync.set({ 'syncedNote': 'syncは約10KB, saveは約5MBまで保存できます\n10KBは約5000文字です' }, () => {
-                    sync0();
-                })
-            } else {
-                localStorage.memo0 = result.syncedNote
-                startScript()
-                console.log('syncedNoteを読み込みました')
-            }
-        })
+    let setObj = { syncedNote: localStorage.memo0 };
 
-    } else {
-        save0()
-
-        let setObj = { 'syncedNote': localStorage.memo0 }
-
-        chrome.storage.sync.set(setObj, () => {
-            if (chrome.runtime.lastError) {
-                alert('データが大きすぎます。saveなら5MB以下, syncなら10KB以下 におさめてください');
-                console.warn('データが大きすぎます。saveなら5MB以下, syncなら10KB以下 におさめてください')
-            }
-            chrome.storage.sync.get("syncedNote", r => { console.log('同期する内容を"' + r.syncedNote + '"に変更しました') })
-        })
-    }
-}
+    chrome.storage.sync.set(setObj, () => {
+      if (chrome.runtime.lastError) {
+        alert(
+          "データが大きすぎます。saveなら5MB以下, syncなら10KB以下 におさめてください"
+        );
+        console.warn(
+          "データが大きすぎます。saveなら5MB以下, syncなら10KB以下 におさめてください"
+        );
+      }
+      chrome.storage.sync.get("syncedNote", (r) => {
+        console.log('同期する内容を"' + r.syncedNote + '"に変更しました');
+      });
+    });
+  }
+};
 
 /**
  * localStorage.memo0 と､フォームの内容を削除する
  */
 const reset0 = () => {
-    localStorage.removeItem('memo0');
-    document.getElementById('memo0').value = '';
-    startScript()
-}
+  localStorage.removeItem("memo0");
+  document.getElementById("memo0").value = "";
+  startScript();
+};
 
 /**
  * memo0(テキストエリア) を範囲選択してコピーする
  */
 const copy0 = () => {
-    document.getElementById('memo0').select();
-    document.execCommand('copy');
-}
+  document.getElementById("memo0").select();
+  document.execCommand("copy");
+};
 
-startScript()
+startScript();
 
-document.getElementById('save').addEventListener('click', save0)
-document.getElementById('sync').addEventListener('click', sync0)
-document.getElementById('reset').addEventListener('click', reset0)
-document.getElementById('copy').addEventListener('click', copy0)
+document.getElementById("save").addEventListener("click", save0);
+document.getElementById("sync").addEventListener("click", sync0);
+document.getElementById("reset").addEventListener("click", reset0);
+document.getElementById("copy").addEventListener("click", copy0);
--- home_time.js
+++ home_time.js
@@ -1,21 +1,20 @@
-const plus0 = num => {
-    let plus0
-    if (num < 10) {
-        plus0 = "0" + num
-    } else {
-        plus0 = num
-    } return plus0
-}
+const plus0 = (num) => {
+  let plus0;
+  if (num < 10) {
+    plus0 = "0" + num;
+  } else {
+    plus0 = num;
+  }
+  return plus0;
+};
 const clock = () => {
-        const now = new Date();
+  const now = new Date();
 
-        let month = plus0(now.getMonth() + 1)
-        let hours = plus0(now.getHours())
-        let Minutes = plus0(now.getMinutes())
-        
+  let month = plus0(now.getMonth() + 1);
+  let hours = plus0(now.getHours());
+  let Minutes = plus0(now.getMinutes());
 
-        document.getElementById('clock_time').textContent 
-        = hours + ":" + Minutes
-}
-clock()
-setInterval(clock, 1000)
+  document.getElementById("clock_time").textContent = hours + ":" + Minutes;
+};
+clock();
+setInterval(clock, 1000);
